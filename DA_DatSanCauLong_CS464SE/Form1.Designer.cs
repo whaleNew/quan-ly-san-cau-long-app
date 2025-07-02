c5364dc9d1e1056702957273be708460df1218dd
Trieu Dep Trai <trantrieudaika@gmail.com>
2025-07-02 10:19:43 +0700

    Add form login

--- DA_DatSanCauLong_CS464SE/Form1.Designer.cs
+++ DA_DatSanCauLong_CS464SE/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tia = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_tk = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.btn_Fresh = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(427, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng Nhập";
+            // 
+            // Tia
+            // 
+            this.Tia.AutoSize = true;
+            this.Tia.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tia.Location = new System.Drawing.Point(219, 150);
+            this.Tia.Name = "Tia";
+            this.Tia.Size = new System.Drawing.Size(94, 22);
+            this.Tia.TabIndex = 1;
+            this.Tia.Text = "Tài khoản:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(225, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // txt_tk
+            // 
+            this.txt_tk.Location = new System.Drawing.Point(388, 150);
+            this.txt_tk.Name = "txt_tk";
+            this.txt_tk.Size = new System.Drawing.Size(284, 22);
+            this.txt_tk.TabIndex = 3;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(388, 238);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(284, 22);
+            this.txt_pass.TabIndex = 4;
+            // 
+            // btn_Fresh
+            // 
+            this.btn_Fresh.BackColor = System.Drawing.Color.DarkGray;
+            this.btn_Fresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_Fresh.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.btn_Fresh.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Fresh.ForeColor = System.Drawing.Color.Black;
+            this.btn_Fresh.Location = new System.Drawing.Point(248, 322);
+            this.btn_Fresh.Name = "btn_Fresh";
+            this.btn_Fresh.Size = new System.Drawing.Size(117, 46);
+            this.btn_Fresh.TabIndex = 15;
+            this.btn_Fresh.Text = "Nhập lại";
+            this.btn_Fresh.UseVisualStyleBackColor = false;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.BackColor = System.Drawing.Color.DarkGray;
+            this.btn_Exit.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btn_Exit.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Exit.ForeColor = System.Drawing.Color.Black;
+            this.btn_Exit.Location = new System.Drawing.Point(687, 322);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(112, 46);
+            this.btn_Exit.TabIndex = 14;
+            this.btn_Exit.Text = "Thoát";
+            this.btn_Exit.UseVisualStyleBackColor = false;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.DarkGray;
+            this.btn_Login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_Login.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.btn_Login.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.ForeColor = System.Drawing.Color.Black;
+            this.btn_Login.Location = new System.Drawing.Point(465, 322);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(119, 46);
+            this.btn_Login.TabIndex = 13;
+            this.btn_Login.Text = "Đăng nhập";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1028, 591);
+            this.Controls.Add(this.btn_Fresh);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_tk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Tia);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Tia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_tk;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Button btn_Fresh;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Login;
     }
 }
 
